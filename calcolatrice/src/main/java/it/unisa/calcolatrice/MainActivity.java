89cc8d64011a84a59af30bc1eb8dcc1eb99b7291
MicheleRabesco <m.rabesco@studenti.unisa.it>
2023-02-06 10:18:43 +0100

    actually works, but you can do one operation at a time

--- calcolatrice/src/main/java/it/unisa/calcolatrice/MainActivity.java
+++ calcolatrice/src/main/java/it/unisa/calcolatrice/MainActivity.java
@@ -3,12 +3,183 @@ package it.unisa.calcolatrice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView operationDisplay;
+    private TextView resultDisplay;
+    private TextView memoryDisplay;
+    private Button clear;
+    private Button memoryClear;
+    private Button memorySave;
+    private Button memoryRecall;
+    private Button subtract;
+    private Button sum;
+    private Button multi;
+    private Button divide;
+    private Button n1;
+    private Button n2;
+    private Button n3;
+    private Button n4;
+    private Button n5;
+    private Button n6;
+    private Button n7;
+    private Button n8;
+    private Button n9;
+    private Button n0;
+    private Button dot;
+    private Button equal;
+
+    private Boolean equalJustPressed = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        resultDisplay = findViewById(R.id.resultDisplay);
+        operationDisplay = findViewById(R.id.operationDisplay);
+        findViewById(R.id.n0).setOnClickListener(this);
+        findViewById(R.id.n1).setOnClickListener(this);
+        findViewById(R.id.n2).setOnClickListener(this);
+        findViewById(R.id.n3).setOnClickListener(this);
+        findViewById(R.id.n4).setOnClickListener(this);
+        findViewById(R.id.n5).setOnClickListener(this);
+        findViewById(R.id.n6).setOnClickListener(this);
+        findViewById(R.id.n7).setOnClickListener(this);
+        findViewById(R.id.n8).setOnClickListener(this);
+        findViewById(R.id.n9).setOnClickListener(this);
+        findViewById(R.id.sum).setOnClickListener(this);
+        findViewById(R.id.subtract).setOnClickListener(this);
+        findViewById(R.id.multi).setOnClickListener(this);
+        findViewById(R.id.divide).setOnClickListener(this);
+        findViewById(R.id.equal).setOnClickListener(this);
+        findViewById(R.id.clear).setOnClickListener(this);
+
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.n0:
+            case R.id.n1:
+            case R.id.n2:
+            case R.id.n3:
+            case R.id.n4:
+            case R.id.n5:
+            case R.id.n6:
+            case R.id.n7:
+            case R.id.n8:
+            case R.id.n9:
+                operationDisplay.append(((Button) v).getText());
+                break;
+            case R.id.sum:
+            case R.id.subtract:
+            case R.id.multi:
+            case R.id.divide:
+                operationDisplay.append(" " + ((Button) v).getText() + " ");
+                break;
+            case R.id.equal:
+                String equation = operationDisplay.getText().toString();
+
+                String[] operands = equation.split("[\\+\\-\\*\\/]");
+                int op1 = Integer.parseInt(operands[0].trim());
+                int op2 = Integer.parseInt(operands[1].trim());
+                float result = 0;
+
+                if (equation.contains("+")) {
+                    result = op1 + op2;
+                    //resultDisplay.setText(Float.toString(result));
+                    equalJustPressed = true;
+                } else if (equation.contains("-")) {
+                    result = op1 - op2;
+                    //resultDisplay.setText(Float.toString(result));
+                    equalJustPressed = true;
+                } else if (equation.contains("*")) {
+                    result = op1 * op2;
+                    //resultDisplay.setText(Float.toString(result));
+                    equalJustPressed = true;
+                } else if (equation.contains("/")) {
+                    result = op1 / op2;
+                    //resultDisplay.setText(Float.toString(result));
+                    equalJustPressed = true;
+                }
+                resultDisplay.setText(String.valueOf(result));
+                break;
+            case R.id.clear:
+                operationDisplay.setText("");
+                resultDisplay.setText("");
+                break;
+
+        }
+        /*
+        if (equalJustPressed) {
+            switch (v.getId()) {
+                case R.id.n0:
+                case R.id.n1:
+                case R.id.n2:
+                case R.id.n3:
+                case R.id.n4:
+                case R.id.n5:
+                case R.id.n6:
+                case R.id.n7:
+                case R.id.n8:
+                case R.id.n9:
+                    operationDisplay.append(((Button) v).getText());
+                    break;
+                case R.id.sum:
+                case R.id.subtract:
+                case R.id.multi:
+                case R.id.divide:
+                    operationDisplay.append(" " + ((Button) v).getText() + " ");
+                    break;
+                case R.id.equal:
+                    String equation = operationDisplay.getText().toString();
+
+                    String[] operands = equation.split("[\\+\\-\\*\\/]");
+                    int op1 = Integer.parseInt(operands[0].trim());
+                    int op2 = Integer.parseInt(operands[1].trim());
+                    float result = 0;
+
+                    if (equation.contains("+")) {
+                        result = op1 + op2;
+                        //resultDisplay.setText(Float.toString(result));
+                        equalJustPressed = true;
+                    } else if (equation.contains("-")) {
+                        result = op1 - op2;
+                        //resultDisplay.setText(Float.toString(result));
+                        equalJustPressed = true;
+                    } else if (equation.contains("*")) {
+                        result = op1 * op2;
+                        //resultDisplay.setText(Float.toString(result));
+                        equalJustPressed = true;
+                    } else if (equation.contains("/")) {
+                        result = op1 / op2;
+                        //resultDisplay.setText(Float.toString(result));
+                        equalJustPressed = true;
+                    }
+                    resultDisplay.setText(String.valueOf(result));
+                    break;
+                case R.id.clear:
+                    operationDisplay.setText("");
+                    resultDisplay.setText("");
+                    break;
+            }
+
+        }
+        */
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
     }
 }
