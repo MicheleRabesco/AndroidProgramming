8947e5e12a9e1426e4929bb7d9eff75e3586795a
MicheleRabesco <m.rabesco@studenti.unisa.it>
2023-02-07 10:09:30 +0100

    Fragments creation

---
+++ fragments/src/main/java/it/unisa/fragments/FragmentsMain.java
@@ -0,0 +1,14 @@
+package it.unisa.fragments;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FragmentsMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragments_main);
+    }
+}
